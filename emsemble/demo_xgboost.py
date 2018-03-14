7cbbee2b09cf6eef5af5f14314163172c966c22b
rfouyang <rfouyang@gmail.com>
2018-03-14 16:43:03 +0800

    first

--- emsemble/demo_xgboost.py
+++ emsemble/demo_xgboost.py
@@ -1,4 +1,4 @@
-
+import matplotlib.pyplot as plt
 import sklearn.model_selection
 import sklearn.datasets
 import sklearn.metrics
@@ -27,10 +27,21 @@ def get_data_boston():
     info = sklearn.datasets.load_boston()
     xs = info['data']
     ys = info['target']
+    print(info)
 
     xtrain, xtest, ytrain, ytest = sklearn.model_selection.train_test_split(xs, ys, test_size=0.2)
 
     return xtrain, xtest, ytrain, ytest
+
+def get_data_boston_with_fname():
+    info = sklearn.datasets.load_boston()
+    xs = info['data']
+    ys = info['target']
+    fname = info['feature_names']
+
+    xtrain, xtest, ytrain, ytest = sklearn.model_selection.train_test_split(xs, ys, test_size=0.2)
+
+    return xtrain, xtest, ytrain, ytest, fname
 
 
 def multiclassification_xgboost():
@@ -155,11 +166,58 @@ def regression_xgboost():
     mes = sklearn.metrics.mean_squared_error(ytest, ypred)
     print(mes)
 
-def main():
-    regression_xgboost()
-    binaryclassification_xgboost()
-    multiclassification_xgboost()
+def feature_importance_xgboost():
+    params = dict()
+    params['eta'] = 0.3
+    params['min_child_weight'] = 10
+    params['cosample_bytree'] = 0.8
+    params['max_depth'] = 5
+    params['subsample'] = 0.5
+    params['gamma'] = 2.0
+    params['alpha'] = 1.0
 
+    config = {
+        'eval_metric': 'rmse',
+        'objective': 'reg:linear',
+        'nthread': 4,
+        'booster': 'gbtree',
+        'tree_method': 'exact',
+        'silent': 1
+    }
+
+    config = {**config, **params}
+
+    xtrain, xtest, ytrain, ytest, fname = get_data_boston_with_fname()
+
+    dtrain = xgb.DMatrix(xtrain, label=ytrain)
+    dtest = xgb.DMatrix(xtest, label=ytest)
+
+    evallist = [(dtrain, 'train'), (dtest, 'test')]
+    num_boost_round = 10
+    model = xgb.train(config, dtrain, num_boost_round, evals=evallist, early_stopping_rounds=100, verbose_eval=True)
+
+    fmap_fp = 'fmap.txt'
+    f = open(fmap_fp, 'w')
+    for i, feature in enumerate(fname):
+        f.write('{0}\t{1}\tq\n'.format(i, feature))
+    f.close()
+
+    feature_weights = model.get_fscore(fmap=fmap_fp)
+    feature_weights = sorted(feature_weights.items(), key=lambda x: x[1], reverse=True)
+    print(feature_weights)
+
+    model.save_model('model.bin')
+    model.dump_model('desc.txt', fmap=fmap_fp)
+
+    xgb.plot_tree(model, fmap=fmap_fp, num_trees=1)
+    plt.show()
+
+
+def main():
+    #regression_xgboost()
+    #binaryclassification_xgboost()
+    #multiclassification_xgboost()
+    feature_importance_xgboost()
 
 if __name__ == '__main__':
     main()
